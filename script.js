691c15882d774ef5ab70f42bc91e18be19f82ca5
Vanessa <vangalepark@gmail.com>
2019-12-04 19:28:09 -0600

    Pre bonus missions

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+    response.json().then( function(json){
+      const container = document.getElementById("container")
+      let index = 0
+      for (let i = json.length; index < i; index++) {
+        container.innerHTML +=`
+          <div class = "astronaut">
+            <ul>
+              <h3>${json[index].firstName}, ${json[index].lastName}</h3>
+              <p>Astronaut Number: ${json[index].id}</p>
+              <li>Hours in space: ${json[index].hoursInSpace}</li>
+              <li>Active: ${json[index].active}</li>
+              <li>Skills: ${json[index].skills}</li>
+            </ul>
+            <img class="avatar" src=${json[index].picture} </img>
+
+          </div>
+        `;
+      }
+    })
+  })
+})
