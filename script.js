5fa954a3d2a52df09f7e07d2d4662af41b603b6d
Vanessa <vangalepark@gmail.com>
2019-12-04 20:24:41 -0600

    Made active true green and active false red

--- script.js
+++ script.js
@@ -4,18 +4,33 @@ window.addEventListener("load", function() {
       const container = document.getElementById("container")
       let index = 0
       for (let i = json.length; index < i; index++) {
-        container.innerHTML +=`
-          <div class = "astronaut">
-            <ul>
-              <h3>${json[index].firstName}, ${json[index].lastName}</h3>
-              <p>Astronaut Number: ${json[index].id}</p>
-              <li>Hours in space: ${json[index].hoursInSpace}</li>
-              <li>Active: ${json[index].active}</li>
-              <li>Skills: ${json[index].skills}</li>
-            </ul>
-            <img class="avatar" src=${json[index].picture} </img>
-          </div>
-        `;
+        if(json[index].active === true) {
+          container.innerHTML +=`
+            <div class = "astronaut">
+              <ul>
+                <h3>${json[index].firstName}, ${json[index].lastName}</h3>
+                <p>Astronaut Number: ${json[index].id}</p>
+                <li>Hours in space: ${json[index].hoursInSpace}</li>
+                <li style="color:green">Active: ${json[index].active}</li>
+                <li>Skills: ${json[index].skills}</li>
+              </ul>
+              <img class="avatar" src=${json[index].picture} </img>
+            </div>
+          `;
+        } else {
+          container.innerHTML +=`
+            <div class = "astronaut">
+              <ul>
+                <h3>${json[index].firstName}, ${json[index].lastName}</h3>
+                <p>Astronaut Number: ${json[index].id}</p>
+                <li>Hours in space: ${json[index].hoursInSpace}</li>
+                <li style="color:red">Active: ${json[index].active}</li>
+                <li>Skills: ${json[index].skills}</li>
+              </ul>
+              <img class="avatar" src=${json[index].picture} </img>
+            </div>
+            `;
+        }
       }
     })
   })
